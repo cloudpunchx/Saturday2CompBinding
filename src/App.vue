aabfb398661df06cb52642e78776bb7ea62bcc01
cloudpunchx <siobhanbonardi@hotmail.com>
2022-12-03 11:15:51 -0700

    Started new vue project, learning reactive binding.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CompBinding/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CompBinding from './components/CompBinding.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CompBinding,
   }
 }
 </script>
---
+++ src/components/CompBinding.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <!-- the mustache syntax, in here you can actually write any valid JS (just don't make it too big/complicated)-->
+        {{counter}}
+        <!-- ternary operators can be put here, where value depends on a condition -->
+        {{counter > 5 ? "large" : "small"}}
+        <button>Button Increment</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "CompBinding",
+        data() {
+            return {
+                counter: 0
+            }
+        },
+        methods: {
+            increment() {
+                this.counter++
+            }
+        },
+        mounted () {
+            document.querySelector(`button`).addEventListener(`click`, this.increment);
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
